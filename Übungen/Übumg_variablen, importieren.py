67b921d2098d897bdf48f5923a95823d34095509
Lennard Holzinger <jts@mint-main-tauber.de>
2026-04-15 15:33:59 +0200

    kleine enderungen

--- Übungen/Übumg_variablen, importieren.py
+++ Übungen/Übumg_variablen, importieren.py
@@ -8,12 +8,16 @@ from random import randint
 #localtime() gibt die aktuelle zeit zurück
 print(time.localtime())
 
+print("\n")
+
 # variablen
 name = "Max"
 alter = 15
 ist_schule = True
 list = [1, 2, 3, 4, 5]
 randzahl = randint(1, 100)
+
+print("\n")
 
 # ausgabe von variablen
 print(name)
@@ -22,24 +26,21 @@ print(ist_schule)
 print(list)
 print(randzahl)
 
-# verwenden von funktionen
+print("\n")
 
+# verwenden von funktionen
 def begruessung(name):
     print("Hallo " + name + "!")
 
+begruessung("Max")
+
+print("\n")
 
 # for-schleife
 for i in range(10):
     print(i)
 
-
-
-
-
-
-print(time.time())
-print(randint(1, 10))
-
+print("\n")
 
 
 
