677f92842f503b0ef0624df08d90c6d9961b0740
Lennard Holzinger <jts@mint-main-tauber.de>
2026-05-20 15:53:19 +0200

    ergenzunng

---
+++ Übungen/fack Hacker.py
@@ -0,0 +1,20 @@
+import random, time
+
+print()
+def zahle_gen():
+    x1 = (f"\033[92m{0}")
+    x2 = (f"\033[92m{1}")
+    x = random.choice([x1, x2])
+    return x
+
+
+while True:
+    time.sleep(0.0005)
+    print(zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),
+          zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),
+          zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),
+          zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),
+          zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),
+          zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),
+          zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen(),
+          zahle_gen(),zahle_gen(),zahle_gen(),zahle_gen())
