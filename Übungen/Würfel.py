1afd256dfdc3123b4bbf63f75d116d69bf44871b
Lennard Holzinger <lennholz09@gmail.com>
2026-06-30 16:06:49 +0200

    würfel spiel

---
+++ Übungen/Würfel.py
@@ -0,0 +1,18 @@
+import random
+
+print("Würfel")
+
+#while True:
+gewürfelte_zahl = random.randint(1, 6)
+
+print("\nDer Würfel hat " + str(gewürfelte_zahl) + " gezeigt.")
+
+#user_input = input("\nMöchten Sie noch einmal würfeln? (j/n): ")
+
+#if user_input.lower() == "j":
+#    print("nochmal würfeln")
+#elif user_input.lower() == "n":
+#    print("Auf Wiedersehen!")
+#    break
+#else:
+#    print("Ungültige Eingabe. Bitte geben Sie 'j' für Ja oder 'n' für Nein ein.")
