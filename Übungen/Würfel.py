c06844ca10b5fd0edd1c87ff7c425ba9258afe45
Lennard Holzinger <lennholz09@gmail.com>
2026-06-30 16:22:51 +0200

    würfel

--- Übungen/Würfel.py
+++ Übungen/Würfel.py
@@ -5,7 +5,7 @@ print("Würfel")
 #while True:
 gewürfelte_zahl = random.randint(1, 6)
 
-print("\nDer Würfel hat " + str(gewürfelte_zahl) + " gezeigt.")
+print("\nDer Würfel hat " + str(gewürfelte_zahl) + " gewürfelt.")
 
 #user_input = input("\nMöchten Sie noch einmal würfeln? (j/n): ")
 
